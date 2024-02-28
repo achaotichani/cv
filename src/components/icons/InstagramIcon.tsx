d7db7f45abc4f2b60d30fd5b95c0f37dc8cb906d
Ngo <haningo2608@gmail.com>
2024-02-28 18:18:31 +0700

    updated the instagram icon

--- src/components/icons/InstagramIcon.tsx
+++ src/components/icons/InstagramIcon.tsx
@@ -3,7 +3,7 @@ export const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => {
     <svg
       style={{ color: 'rgb(107, 114, 128)' }}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="40 40 500 500"
+      viewBox="10 30 500 500"
       {...props}
     >
       <title>Instagram</title>
